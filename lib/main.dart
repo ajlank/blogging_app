9439df90e371006fdcd9e1c07d6bfb4756634168
Ajlan Khan <105883399+ajlank@users.noreply.github.com>
2026-03-17 13:01:06 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required MockFirebaseAuth firebaseAuth});
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
