72678d7dfad5cba7e6116abb3aea1df706d7d30f
Md Asif Khan <asif.khan08@northsouth.edu>
2026-03-17 10:59:17 +0600

    issue fixed 5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:blog_app/blogging.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:blog_app/main.dart';
 
