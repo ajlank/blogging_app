8ea8d9b694c0afd3742c0235d1aa2d539caa231b
Md Asif Khan <asif.khan08@northsouth.edu>
2026-03-17 10:56:02 +0600

    issue fixed 4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,34 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:blog_app/blogging.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:blog_app/main.dart';
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    // Use fake options for CI/CD tests
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'fake_key',
+        appId: 'fake_appId',
+        messagingSenderId: 'fake_senderId',
+        projectId: 'fake_projectId',
+      ),
+    );
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify initial counter
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap + and rebuild
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    // Verify increment
     expect(find.text('1'), findsOneWidget);
   });
 }
